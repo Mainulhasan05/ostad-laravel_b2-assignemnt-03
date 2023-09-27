ba447973ce8f98deee00c3b451f9d953d3cb9f75
Md. Mainul Hasan <mdrifatbd5@gmail.com>
2023-09-27 23:03:21 +0600

    password generator

--- task_5.php
+++ task_5.php
@@ -1,10 +1,19 @@
 <?php
 function generatePassword($length){
     $result="";
-    $characters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
-    for($i=0;$i<$length;$i++){
-        $result.=$characters[rand(0,strlen($characters)-1)];
+    $capital="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $small="abcdefghijklmnopqrstuvwxyz";
+    $number="0123456789";
+    $special="!@#$%^&*()_-=+;:,.?";
+
+    $result.=$capital[rand(0,strlen($capital)-1)];
+    $result.=$small[rand(0,strlen($small)-1)];
+    $result.=$number[rand(0,strlen($number)-1)];
+    $result.=$special[rand(0,strlen($special)-1)];
+    for($i=0;$i<$length-4;$i++){
+        $result.=$capital[rand(0,strlen($capital)-1)];
     }
-    return $result;
+    // return $result;
+    return str_shuffle($result);
 }
 echo generatePassword(12)."\n";
